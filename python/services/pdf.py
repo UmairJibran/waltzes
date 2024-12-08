5aa67e2660176127bba3187e47cfef685f75bd9f
Umair Jibran <me@umairjibran.com>
2024-12-08 14:58:29 +0500

    dynamise header title

--- python/services/pdf.py
+++ python/services/pdf.py
@@ -1,15 +1,15 @@
 from fpdf import FPDF
 
 class PDF(FPDF):
-    # receive name in the constructor
-    def __init__(self, name):
-        self.name = name;
-        print(self);
+    def __init__(self, title=None):
+        super().__init__()
+        self.title = title
 
     def header(self):
-        self.set_font('Arial', 'BI', 12)
-        self.cell(0, 10, 'Umair Jibran', 0, 0, 'C')
-        self.ln(20)
+        if self.title:
+            self.set_font('Arial', 'BI', 12)
+            self.cell(0, 10, self.title, 0, 0, 'C')
+            self.ln(20)
 
     def footer(self):
         self.set_y(-15)
@@ -18,7 +18,7 @@ class PDF(FPDF):
 
 
 def convert_text_to_pdf(text):
-    pdf = PDF("Umair Jibran")
+    pdf = PDF("Umair Jibran - Productbox")
     pdf.alias_nb_pages()
     pdf.add_page()
     pdf.set_font('Times', '', 12)
