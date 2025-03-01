7e57563257a1cba134a99a37941e6a39b75d4441
Umair Jibran <me@umairjibran.com>
2025-03-01 15:56:29 +0500

    update utils

--- python/app.py
+++ python/app.py
@@ -15,7 +15,8 @@ from main import (
 from services.resume_best_match import get_best_match_from_resume
 from services.resume_vectorizor import vectorize_resume
 from services.openai import call_openai_api, generate_cover_letter
-from utils import parse_json_from_llm
+from utils.utils import parse_json_from_llm
+from utils.prompts import system_prompt_linkedin_profile_segmentation
 
 
 app = Flask(__name__)
@@ -131,61 +132,7 @@ def convert_resume():
     if user_linkedin_data is None:
         return ("Please scrape your LinkedIn data first", 400)
 
-    system_prompt = """
-        You are a head hunter with over 10 years of experience in the recruitment industry.
-        You have a proven track record of successfully placing candidates in various roles across different industries.
-        You are passionate about helping people find their dream jobs and thrive in their careers.
-        You will be given a user's LinkedIn data, which will include most of the information needed to create a resume.
-        You will need to generate resume segments based on the user's LinkedIn data.
-
-        Keep in mind that the user's LinkedIn data may not be complete, but what is available should be used to generate the resume segments.
-        These segments should be very detailed and include all relevant information from the user's LinkedIn profile and the provided job posting Which will also be provided to you.
-
-        The Resume should be very well worded and should be able to pass through the ATS system.
-
-        The segments should be a json object with the following structure (NOT MARKDOWN, but a JSON object's string):
-        {
-            "name": "user's full name",
-            "contact": [
-                "site (if available)",
-                "email",
-                "phone (if available)",
-                "linkedin_url",
-            ],
-            "experience": [
-                {
-                    "title": "job title",
-                    "location": "job location",
-                    "date": "from - to/present",
-                    "description": ["bullet point 1", "bullet point 2", ...],
-                }
-            ],
-            "education": [
-                {
-                    "title": "degree",
-                    "location": "university",
-                    "date": "from - to",
-                    "description": ["bullet point 1", "bullet point 2", ...],
-                }
-            ],
-            "skills": ["skill 1", "skill 2", ...],
-            "certifications": [
-                {
-                    "title": "certification title",
-                    "date": "date",
-                    "description": "description",
-                }
-            ],
-            "open_source": [
-                {
-                    "title": "project title",
-                    "description": "project description",
-                    "link": "project link",
-                }
-            ],
-        }
-    """
-
+    system_prompt = system_prompt_linkedin_profile_segmentation
     job_details = ""
     if "greenhouse" in job_url:
         job_details = fetch_job_details_from_greenhouse(job_url)
