48d94402ba24a0c2b1174737573ee5d8c12b9cde
Umair Jibran <me@umairjibran.com>
2025-03-02 00:29:51 +0500

    init plugin with react

---
+++ addon/src/pages/Popup/Popup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greetings from '../../containers/Greetings/Greetings';
+import './Popup.css';
+
+const Popup = () => {
+  return (
+    <div className="App">
+      <Greetings />
+    </div>
+  );
+};
+
+export default Popup;
---
+++ addon/src/pages/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greetings from '../../containers/Greetings/Greetings';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div className="App">
+      <Greetings />
+    </div>
+  );
+};
+
+export default Sidebar;
