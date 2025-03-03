bd9a0f70b890901f912d1c6e992e80aad35363ca
Umair Jibran <me@umairjibran.com>
2025-03-04 00:09:35 +0500

    create user entity

--- node-server/src/users/entities/user.entity.ts
+++ node-server/src/users/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+export class User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone?: string;
+  portfolioUrl?: string;
+  linkedinUsername?: string;
+  githubUsername?: string;
+  additionalInstructions?: string;
+  password: string;
+  role: 'admin' | 'user';
+  createdAt: Date;
+  updatedAt: Date;
+}
