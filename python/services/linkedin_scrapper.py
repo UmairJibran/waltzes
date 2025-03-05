f91d7c1c9160cb3b441b9cc019f706516f2e2dac
Umair Jibran <me@umairjibran.com>
2025-03-05 20:13:07 +0500

    update other scrapers to return similar response

--- python/services/linkedin_scrapper.py
+++ python/services/linkedin_scrapper.py
@@ -14,20 +14,37 @@ def fetch_user_linkedin_scraping_dog(linkedin_username: str):
         linkedin_data_raw = {
             "first_name": data.get("first_name", None),
             "last_name": data.get("last_name", None),
+            "full_name": f"{data.get('first_name', '')} {data.get('last_name', '')}".strip()
+            or None,
+            "occupation": data.get("headline", None),
             "headline": data.get("headline", None),
             "location": data.get("location", None),
             "about": data.get("about", None),
+            "country": None,
+            "country_full_name": None,
+            "city": None,
+            "state": None,
+            "skills": data.get("skills", None),
             "experience": data.get("experience", None),
             "education": data.get("education", None),
+            "languages_and_proficiencies": data.get("languages", None),
+            "accomplishment_organisations": None,
+            "accomplishment_publications": data.get("publications", None),
+            "accomplishment_honors_awards": data.get("awards", None),
+            "accomplishment_courses": data.get("courses", None),
+            "accomplishment_patents": None,
+            "accomplishment_projects": data.get("projects", None),
+            "accomplishment_test_scores": None,
+            "volunteer_work": data.get("activities", None),
+            "recommendations": None,
+            "certifications": None,
             "activities": data.get("activities", None),
-            "publications": data.get("publications", None),
-            "courses": data.get("courses", None),
-            "languages": data.get("languages", None),
-            "projects": data.get("projects", None),
-            "awards": data.get("awards", None),
-            "skills": data.get("skills", None),
+            "articles": None,
+            "industry": None,
+            "extra": None,
+            "interests": None,
         }
-        return {"linkedin_data_raw": linkedin_data_raw}
+        return linkedin_data_raw
     except requests.RequestException as e:
         print(f"Error from Scraping Dog: {e}")
         return None
@@ -43,26 +60,51 @@ def fetch_user_linkedin_scrapin_io(linkedin_username: str):
             return None
         data = response.json()
         profile = data.get("person", {})
+        company = profile.get("company", {})
+
         linkedin_data_raw = {
             "first_name": profile.get("firstName", None),
             "last_name": profile.get("lastName", None),
+            "full_name": f"{profile.get('firstName', '')} {profile.get('lastName', '')}".strip()
+            or None,
+            "occupation": profile.get("headline", None),
             "headline": profile.get("headline", None),
             "location": profile.get("location", None),
             "about": profile.get("summary", None),
+            "country": None,
+            "country_full_name": None,
+            "city": None,
+            "state": None,
             "skills": profile.get("skills", None),
             "experience": profile.get("positions", None),
             "education": profile.get("schools", None),
-            "languages": profile.get("languages", None),
+            "languages_and_proficiencies": profile.get("languages", None),
+            "accomplishment_organisations": None,
+            "accomplishment_publications": None,
+            "accomplishment_honors_awards": None,
+            "accomplishment_courses": None,
+            "accomplishment_patents": None,
+            "accomplishment_projects": None,
+            "accomplishment_test_scores": None,
+            "volunteer_work": profile.get("volunteeringExperiences", None),
+            "recommendations": None,
+            "certifications": profile.get("certifications", None),
             "activities": profile.get("volunteeringExperiences", None),
-            "awards": profile.get("certifications", None),
-            "current_company": {
-                "name": profile.get("company", {}).get("name", None),
-                "description": profile.get("company", {}).get("description", None),
-                "industry": profile.get("company", {}).get("industry", None),
-                "specialities": profile.get("company", {}).get("specialities", None),
-            },
+            "articles": None,
+            "industry": company.get("industry", None),
+            "extra": {
+                "current_company": {
+                    "name": company.get("name", None),
+                    "description": company.get("description", None),
+                    "industry": company.get("industry", None),
+                    "specialities": company.get("specialities", None),
+                }
+            }
+            if company
+            else None,
+            "interests": None,
         }
-        return {"linkedin_data_raw": linkedin_data_raw}
+        return linkedin_data_raw
     except requests.RequestException as e:
         print(f"Error from ScrapinIO: {e}")
         return None
