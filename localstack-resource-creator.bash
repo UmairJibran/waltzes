9260c80a45e0cf2efcacebef159740f86a0cd18f
Umair Jibran <me@umairjibran.com>
2025-03-07 23:08:08 +0500

    handle errors gracefully

--- localstack-resource-creator.bash
+++ localstack-resource-creator.bash
@@ -5,18 +5,18 @@ ses_verified_emails=("hello@waltzes.umairjibran.com" "no-reply@waltzes.umairjibr
 
 for queue in "${queues[@]}"
 do
-  awslocal sqs create-queue --queue-name $queue.fifo --attributes FifoQueue=true,ContentBasedDeduplication=true
+  awslocal sqs create-queue --queue-name $queue.fifo --attributes FifoQueue=true,ContentBasedDeduplication=true || exit 1
 done
 
 for bucket in "${buckets[@]}"
 do
-  awslocal s3api create-bucket --bucket $bucket
-  awslocal s3api put-bucket-cors --bucket $bucket --cors-configuration file://local-stack-cors-policy.json
+  awslocal s3api create-bucket --bucket $bucket || exit 1
+  awslocal s3api put-bucket-cors --bucket $bucket --cors-configuration file://local-stack-cors-policy.json || exit 1
 done
 
 for email in "${ses_verified_emails[@]}"
 do
-  awslocal ses verify-email-identity --email-address $email
+  awslocal ses verify-email-identity --email-address $email || exit 1
 done
 
 # populate the email templates
