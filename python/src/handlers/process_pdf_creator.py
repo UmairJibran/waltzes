b81ee52c46cdd6efca4162d984ce7ac77f47b83b
Umair Jibran <me@umairjibran.com>
2025-03-10 00:32:46 +0500

    handle cover letter creator in pdf handler

--- python/src/handlers/process_pdf_creator.py
+++ python/src/handlers/process_pdf_creator.py
@@ -2,7 +2,7 @@
 
 import json
 
-from services.pdf.pdf import create_resume
+from services.pdf.pdf import create_cover_letter, create_resume
 from utils import logger
 from utils.utils import send_data_to_callback_url
 
@@ -15,7 +15,17 @@ def process_pdf_creator_queue_message(sqs_message: str):
     """
     message_body = json.loads(sqs_message.get("Body"))
     logger.info(f"Message Body: {message_body}")
-    create_resume(segments=message_body.get("resume"))
+    job_title = message_body.get("jobDetails").get("title")
+    applicant_details = message_body.get("applicantDetails")
+    firstName = applicant_details.get("firstName")
+    lastName = applicant_details.get("lastName")
+    title = f"{firstName} {lastName} - {job_title}"
+    resume_key = None
+    cover_letter_key = None
+    if "resume" in message_body:
+        create_resume(segments=message_body.get("resume"))
+    if "coverLetter" in message_body:
+        create_cover_letter(text=message_body.get("coverLetter"), title=title)
     callback_url = message_body.get("callbackUrl")
     job_details = {"resumePdf": "xba", "coverLetterPdf": "ssss"}
     send_data_to_callback_url(job_details, callback_url)
