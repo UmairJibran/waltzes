c6272efe2589bdf805854f1c40e57050938c5a2a
Umair Jibran <me@umairjibran.com>
2025-03-09 19:23:55 +0500

    node - communicte new job to sqs for scraping

--- node-server/src/applications/applications.module.ts
+++ node-server/src/applications/applications.module.ts
@@ -3,12 +3,16 @@ import { ApplicationsService } from './applications.service';
 import { ApplicationsController } from './applications.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Application, ApplicationSchema } from './schemas/application.schema';
+import { SqsProducerModule } from 'src/aws/sqs-producer/sqs-producer.module';
+import { JobsModule } from 'src/jobs/jobs.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Application.name, schema: ApplicationSchema },
     ]),
+    SqsProducerModule,
+    JobsModule,
   ],
   controllers: [ApplicationsController],
   providers: [ApplicationsService],
--- node-server/src/applications/applications.service.ts
+++ node-server/src/applications/applications.service.ts
@@ -4,19 +4,66 @@ import { UpdateApplicationDto } from './dto/update-application.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Application } from './schemas/application.schema';
 import { Model } from 'mongoose';
+import { SqsProducerService } from 'src/aws/sqs-producer/sqs-producer.service';
+import { JobsService } from 'src/jobs/jobs.service';
 
 @Injectable()
 export class ApplicationsService {
   constructor(
     @InjectModel(Application.name) private applications: Model<Application>,
+    private readonly jobsService: JobsService,
+    private readonly sqsProducerService: SqsProducerService,
   ) {}
 
-  async create(createApplicationDto: CreateApplicationDto, user: string) {
+  async create(
+    createApplicationDto: CreateApplicationDto,
+    user: string,
+    baseUrl: string,
+  ) {
+    const existingJob = await this.jobsService.findByUrl(
+      createApplicationDto.jobUrl,
+    );
+    let jobId: string = existingJob?._id.toString() || '';
+    if (!existingJob) {
+      const newJob = await this.jobsService.initJob(
+        createApplicationDto.jobUrl,
+        { baseUrl },
+      );
+      jobId = newJob._id.toString();
+    } else if (existingJob.status === 'done') {
+      // TODO: send messages to resume and/or cover letter queues with data
+    } else if (existingJob?.status === 'error') {
+      await this.jobsService.initJob(createApplicationDto.jobUrl, { baseUrl });
+    }
+
     const app = await this.applications.create({
       ...createApplicationDto,
       user,
+      job: jobId,
     });
-    return app;
+
+    return { applicationId: app._id };
+  }
+
+  async getApplication(applicationId: string) {
+    const app = await this.applications.findById({
+      _id: applicationId,
+    });
+
+    if (!app) {
+      return null;
+    }
+
+    return {
+      status: 'finished', // | 'processing' | 'finished',
+      steps: {
+        scraping: 'done', // "done" | 'processing' | 'pending',
+        resume: 'done', // | 'processing' | 'pending',
+        coverLetter: 'done', // | 'processing' | 'pending',
+        pdf: 'done', // | 'processing' | 'pending',
+      },
+      downloadUrls: { resume: 'xyz', coverLetter: 'xyz' },
+    };
   }
 
   async findAll(
@@ -40,7 +87,19 @@ export class ApplicationsService {
     const skip = (page - 1) * pageSize;
 
     const [applications, total] = await Promise.all([
-      this.applications.find(query).skip(skip).limit(pageSize).exec(),
+      this.applications
+        .find(query)
+        .skip(skip)
+        .sort({ createdAt: 'asc' })
+        .populate([
+          {
+            path: 'job',
+            model: 'Job',
+            select: 'title companyName location url',
+          },
+        ])
+        .limit(pageSize)
+        .exec(),
       this.applications.countDocuments(query),
     ]);
 
@@ -82,4 +141,29 @@ export class ApplicationsService {
     );
     return app;
   }
+
+  // async startProcessingByUrl(url: string, jobDetails: Job) {
+  //   const pendingApplications = await this.applications.find({
+  //     jobUrl: url,
+  //   });
+  //   const messages: unknown[] = [];
+  //   for (const app of pendingApplications) {
+  //     messages.push({
+  //       app,
+  //       callbackUrl: '',
+  //       jobDetails: {
+  //         companyName: jobDetails.companyName,
+  //         description: jobDetails.description,
+  //         location: jobDetails.location,
+  //         jobTitle: jobDetails.title,
+  //         skills: jobDetails.skills,
+  //       },
+  //       applicantDetails: {},
+  //     });
+  //   }
+  //   console.log(
+  //     '🚀 ~ ApplicationsService ~ startProcessingByUrl ~ messages:',
+  //     messages,
+  //   );
+  // }
 }
