c8126a519181e5103ce342b6515feaea5c540bb4
Umair Jibran <me@umairjibran.com>
2025-03-13 20:28:53 +0500

    web - add page and link to apply from web

---
+++ web/app/(authenticated)/(dashboard-sub)/applications/new/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Page() {
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <h1 className="text-2xl font-semibold">New Application</h1>
+    </div>
+  );
+}
